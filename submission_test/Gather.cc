0bdb1a4551cd3744a20a220c0a10f38677f610a3
Khemarat Boonyapaluk <korla.march@gmail.com>
2016-07-10 23:24:25 +0700

    Implement Partial BFS

--- submission_test/Gather.cc
+++ submission_test/Gather.cc
@@ -1,22 +1,49 @@
 #include "Role.h"
 #include "queue"
+#include "array"
+#include "math.h"
 
 class Gather : public Role{
     public:
         int anchorX, anchorY;
+        Gather(State _state, int _id = 0, int _x = 0, int _y = 0, int _anchorX = 0, int _anchorY = 0) : Role(State _state, _id, _x, _y){
+            anchorX = _anchorX;
+            anchorY = _anchorY;
+            dis(state.rows,std::vector<int>(state.cols));
+            bfs
+        }
+
         int move(){
 
         }
     private:
         //make a small bfs over a small distance (d)
-        int dis[stats.rows][stats.cols];
+        std::vector<std::vector<int> > dis;
+
+        //check if loc is suitable to bfs to
+        bool bfs_checkLoc(Location loc){
+            return !state.getGrid(loc.row,loc.col).isWater && dis[loc.row][loc.col]==0 && !(loc.row==anchorX&&loc.col==anchorY);
+        }
+
         void bfs(int d){
-            std::queue<std::pair<int,int> > qu;
-            qu.push_back({anchorX,anchorY});
-            while(!qu.empty()){
-                auto pi = qu.front();
+            std::queue<Location> qu;
+            qu.push_back(Location(anchorX,anchorY));
+            dis[anchorX][anchorY] = 0;
+            while(!qu.empty())
+            {
+                auto loc = qu.front();
                 qu.pop();
-                if(pi.)
+                int newd = dis[loc.row][loc.col]+1;
+                //if bfs more than d , break
+                if(newd>d) break;
+                for(int i = 0; i < 4; i++){
+                    Location newloc = state.getLocation(loc,i);
+                    if(checkLoc(newloc))
+                    {
+                        dis[newloc.row][newloc.col] = newd;
+                        qu.push(newloc);
+                    }
+                }
             }
         }
 
